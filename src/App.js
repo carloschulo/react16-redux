92ac6f60400956dacdb423e1cb154fc350562ce7
Carlos Henriquez <carloschulo@gmail.com>
2018-04-13 21:47:58 -0400

    updated components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react';
+import React from "react";
+import { Route, Link } from "react-router-dom";
+import Home from "./containers/home";
+import About from "./containers/about";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello</h1>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/about-us">About</Link>
+      </header>
+
+      <main>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about-us" component={About} />
+      </main>
+    </div>
+  );
+};
 
 export default App;
